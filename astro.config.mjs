ac26b8ac08ab8cffc8dac66d2dd25bd00f661de3
IsaacDrp <isaacdp09@gmail.com>
2025-12-08 23:13:59 -0600

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
     // Aquí autorizamos los dominios para el modo preview
     preview: {
-      allowedHosts: ['synectura.com', 'www.synectura.com']
+      allowedHosts: true
     }
   },
 });
